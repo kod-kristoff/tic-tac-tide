5bef1a941c4b39eec115f61e533f37a3468f5995
Kristoffer Andersson <6058164+kod-kristoff@users.noreply.github.com>
2021-01-20 11:14:23 +0100

    feat: Initial commit

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use tide::{Body, Request};
+use tide_websockets::{Message as WSMessage, WebSocket, WebSocketConnection};
+use futures_util::StreamExt;
+
+
+#[async_std::main]
+async fn main() -> Result<(), std::io::Error> {
+    env_logger::init();
+
+    let mut app = tide::new();
+
+    // serve public dir for assets
+    app.at("/public").serve_dir("./public/")?;
+
+    // index route
+    app.at("/").get(|_| async { Ok(Body::from_file("./public/index.html").await?) });
+
+    // board route
+    app.at("/:id")
+        .with(WebSocket::new(
+            |_req: Request<_>, mut wsc: WebSocketConnection| async move {
+                while let Some(Ok(WSMessage::Text(message))) = wsc.next().await {
+                    println!("{:?}", message);
+                }
+
+                Ok(())
+            },
+        ))
+        .get(|_| async { Ok(Body::from_file("./public/board.html").await?) });
+    
+    let port = std::env::var("PORT").unwrap_or_else(|_| "8080".to_string());
+    let addr = format!("0.0.0.0:{}", port);
+    println!("Starting server listening at {}", addr);
+    app.listen(addr).await?;
+
+    Ok(())
+}
